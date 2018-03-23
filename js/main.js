6f467767a6a5720786ea750545e60e872ff94bd7
Samanta <Arajit.Samanta@ehi.com>
2018-03-23 17:08:26 +0100

    Fix pie chart generation

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $('#nav').affix({
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
 		console.log(top)
-		if(top<-300){
+		if(top<-50){
 			if(index==0){	
 			
 				$('.chart').easyPieChart({
